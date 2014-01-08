479a100944e76f0f721b8e89f8ce9aded6121bdb
Jens Ulferts <j.ulferts@finn.de>
2014-01-08 09:55:00 +0100

    fixes specs broken by changes to allowed

--- spec/controllers/journals_controller_spec.rb
+++ spec/controllers/journals_controller_spec.rb
@@ -57,10 +57,11 @@ describe JournalsController do
   describe :edit do
     describe 'authorization' do
       before do
-        work_package.update_attribute :description, 'description'
-        role.add_permission! *permissions
         member.save and user.reload
         allow(User).to receive(:current).and_return user
+
+        work_package.update_attribute :description, 'description'
+        role.add_permission! *permissions
 
         get :edit, id: work_package.journals.last.id
       end
