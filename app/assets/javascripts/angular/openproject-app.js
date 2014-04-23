cd24b0ff0d24bff90c91148a0626f593f05347cc
Hagen Schink <h.schink@finn.de>
2014-04-23 14:46:59 +0200

    Add angular directives for date/time and authoring

--- app/assets/javascripts/angular/openproject-app.js
+++ app/assets/javascripts/angular/openproject-app.js
@@ -52,7 +52,7 @@ angular.module('openproject.messages', ['openproject.messages.controllers']);
 angular.module('openproject.messages.controllers', []);
 
 // main app
-var openprojectApp = angular.module('openproject', ['ui.select2', 'ui.date', 'openproject.uiComponents', 'openproject.timelines', 'openproject.workPackages', 'openproject.messages', 'ngAnimate']);
+var openprojectApp = angular.module('openproject', ['ui.select2', 'ui.date', 'openproject.uiComponents', 'openproject.timelines', 'openproject.workPackages', 'openproject.messages', 'ngAnimate', 'ngSanitize']);
 
 openprojectApp
   .config(['$locationProvider', '$httpProvider', function($locationProvider, $httpProvider) {
